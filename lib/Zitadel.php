f624eb41b1d8094aad5b191ec781ea028aa601a7
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-03-27 13:00:58 +0200

    Dummit commit to test the workflows

---
+++ lib/Zitadel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Zitadel\Client;
+
+use Zitadel\Client\Api\FeatureServiceApi;
+use Zitadel\Client\Api\IdentityProviderServiceApi;
+use Zitadel\Client\Api\OidcServiceApi;
+use Zitadel\Client\Api\OrganizationServiceApi;
+use Zitadel\Client\Api\SessionServiceApi;
+use Zitadel\Client\Api\SettingsServiceApi;
+use Zitadel\Client\Api\UserServiceApi;
+use Zitadel\Client\Configuration;
+
+class Zitadel
+{
+    public FeatureServiceApi $features;
+    public IdentityProviderServiceApi $idps;
+    public OidcServiceApi $oidc;
+    public OrganizationServiceApi $organizations;
+    public SessionServiceApi $sessions;
+    public SettingsServiceApi $settings;
+    public UserServiceApi $users;
+
+    public function __construct(string $host, string $accessToken)
+    {
+        $config = new Configuration();
+        $config->setHost($host)->setAccessToken($accessToken);
+
+        $this->features = new FeatureServiceApi(null, $config);
+        $this->idps = new IdentityProviderServiceApi(null, $config);
+        $this->oidc = new OidcServiceApi(null, $config);
+        $this->organizations = new OrganizationServiceApi(null, $config);
+        $this->sessions = new SessionServiceApi(null, $config);
+        $this->settings = new SettingsServiceApi(null, $config);
+        $this->users = new UserServiceApi(null, $config);
+    }
+}
