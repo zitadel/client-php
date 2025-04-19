f3cad9e65f0fe6fb4c4eacbd21ef3e6ce090d62a
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-04-19 13:53:34 +0300

    Added more tests to ensure that the web-token-authenticator is thoroughly tested.

--- test/Auth/WebTokenAuthenticatorTest.php
+++ test/Auth/WebTokenAuthenticatorTest.php
@@ -26,6 +26,42 @@ class WebTokenAuthenticatorTest extends OAuthAuthenticatorTest
         $this->assertNotEquals($authenticator->refreshToken()->getToken(), $authenticator->refreshToken()->getToken());
     }
 
+    /**
+     * @throws Exception
+     */
+    public function testRefreshTokenWithRS256(): void
+    {
+        $authenticator = WebTokenAuthenticator::builder(static::$oauthHost, "1", WebTokenAuthenticatorTest::getPrivateKey())
+          ->jwtAlgorithm("RS256")
+          ->build();
+
+        $this->assertNotEmpty($authenticator->getAuthToken(), "Access token should not be empty");
+        $token = $authenticator->refreshToken();
+        $this->assertNotEmpty($token->getToken(), "Access token should not be empty");
+        $this->assertFalse($token->hasExpired(), "Token expiry should be in the future");
+        $this->assertEquals($token->getToken(), $authenticator->getAuthToken());
+        $this->assertEquals($authenticator->getHost()->toString(), static::$oauthHost);
+        $this->assertNotEquals($authenticator->refreshToken()->getToken(), $authenticator->refreshToken()->getToken());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testRefreshTokenWithExtendedLifetime(): void
+    {
+        $authenticator = WebTokenAuthenticator::builder(static::$oauthHost, "1", WebTokenAuthenticatorTest::getPrivateKey())
+          ->tokenLifetimeSeconds(86400)
+          ->build();
+
+        $this->assertNotEmpty($authenticator->getAuthToken(), "Access token should not be empty");
+        $token = $authenticator->refreshToken();
+        $this->assertNotEmpty($token->getToken(), "Access token should not be empty");
+        $this->assertFalse($token->hasExpired(), "Token expiry should be in the future");
+        $this->assertEquals($token->getToken(), $authenticator->getAuthToken());
+        $this->assertEquals($authenticator->getHost()->toString(), static::$oauthHost);
+        $this->assertNotEquals($authenticator->refreshToken()->getToken(), $authenticator->refreshToken()->getToken());
+    }
+
     private static function getPrivateKey(): string
     {
         $config = ['private_key_bits' => 2048, 'private_key_type' => OPENSSL_KEYTYPE_RSA];
