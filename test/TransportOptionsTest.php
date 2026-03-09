5f08ec08ce1d4a17904afda290c89b3964f5ffac
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-03-09 23:53:10 +1100

    Restructure tests: split TransportOptions unit tests from Zitadel integration tests

--- test/TransportOptionsTest.php
+++ test/TransportOptionsTest.php
@@ -2,160 +2,58 @@
 
 namespace Zitadel\Client\Test;
 
-use Docker\Docker;
-use Docker\API\Model\NetworksCreatePostBody;
 use PHPUnit\Framework\TestCase;
-use Testcontainers\Container\GenericContainer;
-use Testcontainers\Container\StartedGenericContainer;
-use Testcontainers\Wait\WaitForHttp;
-use Testcontainers\Wait\WaitForHostPort;
-use Exception;
 use Zitadel\Client\TransportOptions;
-use Zitadel\Client\Zitadel;
 
 class TransportOptionsTest extends TestCase
 {
-    protected static string $host;
-    protected static int $httpPort;
-    protected static int $httpsPort;
-    protected static int $proxyPort;
-    protected static string $caCertPath;
-    private static ?string $networkId = null;
-    private static ?string $networkName = null;
-    private static ?StartedGenericContainer $wiremock = null;
-    private static ?StartedGenericContainer $proxy = null;
-
-    public static function setUpBeforeClass(): void
+    public function testDefaultsReturnsEmpty(): void
     {
-        parent::setUpBeforeClass();
-
-        $fixturesDir = __DIR__ . '/fixtures';
-        self::$caCertPath = $fixturesDir . '/ca.pem';
-
-        $docker = Docker::create();
-        self::$networkName = 'zitadel-test-' . bin2hex(random_bytes(4));
-        $networkBody = new NetworksCreatePostBody();
-        $networkBody->setName(self::$networkName);
-        $response = $docker->networkCreate($networkBody);
-        self::$networkId = $response->getId();
-
-        self::$wiremock = (new GenericContainer("wiremock/wiremock:3.12.1"))
-            ->withName('wiremock')
-            ->withNetwork(self::$networkName)
-            ->withCommand([
-                "--https-port", "8443",
-                "--https-keystore", "/home/wiremock/keystore.p12",
-                "--keystore-password", "password",
-                "--keystore-type", "PKCS12",
-                "--global-response-templating",
-            ])
-            ->withMount($fixturesDir . '/keystore.p12', '/home/wiremock/keystore.p12')
-            ->withMount($fixturesDir . '/mappings', '/home/wiremock/mappings')
-            ->withExposedPorts(8080, 8443)
-            ->start();
-
-        self::$proxy = (new GenericContainer("ubuntu/squid:6.10-24.10_beta"))
-            ->withNetwork(self::$networkName)
-            ->withMount($fixturesDir . '/squid.conf', '/etc/squid/squid.conf')
-            ->withExposedPorts(3128)
-            ->start();
-
-        self::$host = self::$wiremock->getHost();
-        self::$httpPort = self::$wiremock->getMappedPort(8080);
-        self::$httpsPort = self::$wiremock->getMappedPort(8443);
-        self::$proxyPort = self::$proxy->getMappedPort(3128);
-
-        (new WaitForHostPort())
-            ->wait(self::$proxy);
-
-        (new WaitForHttp(self::$httpPort))
-            ->withPath("/__admin/mappings")
-            ->withExpectedStatusCode(200)
-            ->wait(self::$wiremock);
+        $this->assertSame([], TransportOptions::defaults()->toGuzzleOptions());
     }
 
-    public static function tearDownAfterClass(): void
+    public function testInsecureSetsVerifyFalse(): void
     {
-        self::$proxy?->stop();
-        self::$wiremock?->stop();
-        if (self::$networkId !== null) {
-            Docker::create()->networkDelete(self::$networkName);
-        }
-        parent::tearDownAfterClass();
+        $opts = new TransportOptions(insecure: true);
+        $result = $opts->toGuzzleOptions();
+        $this->assertFalse($result['verify']);
     }
 
-    public function testCustomCaCert(): void
+    public function testCaCertPathSetsVerify(): void
     {
-        $zitadel = Zitadel::withClientCredentials(
-            "https://" . self::$host . ":" . self::$httpsPort,
-            "dummy-client",
-            "dummy-secret",
-            new TransportOptions(caCertPath: self::$caCertPath),
-        );
-        $this->assertInstanceOf(Zitadel::class, $zitadel);
+        $caCertPath = __DIR__ . '/fixtures/ca.pem';
+        $opts = new TransportOptions(caCertPath: $caCertPath);
+        $result = $opts->toGuzzleOptions();
+        $this->assertSame($caCertPath, $result['verify']);
     }
 
-    public function testInsecureMode(): void
+    public function testProxyUrlSetsProxy(): void
     {
-        $zitadel = Zitadel::withClientCredentials(
-            "https://" . self::$host . ":" . self::$httpsPort,
-            "dummy-client",
-            "dummy-secret",
-            new TransportOptions(insecure: true),
-        );
-        $this->assertInstanceOf(Zitadel::class, $zitadel);
+        $opts = new TransportOptions(proxyUrl: 'http://proxy:3128');
+        $result = $opts->toGuzzleOptions();
+        $this->assertSame('http://proxy:3128', $result['proxy']);
     }
 
-    public function testDefaultHeaders(): void
+    public function testDefaultHeadersSetsHeaders(): void
     {
-        $zitadel = Zitadel::withClientCredentials(
-            "http://" . self::$host . ":" . self::$httpPort,
-            "dummy-client",
-            "dummy-secret",
-            new TransportOptions(defaultHeaders: ["X-Custom-Header" => "test-value"]),
-        );
-        $this->assertInstanceOf(Zitadel::class, $zitadel);
-
-        $zitadel->settings->getGeneralSettings();
-
-        $verifyContext = stream_context_create([
-            'http' => [
-                'method' => 'POST',
-                'header' => "Content-Type: application/json\r\n",
-                'content' => json_encode([
-                    'url' => '/zitadel.settings.v2.SettingsService/GetGeneralSettings',
-                    'headers' => ['X-Custom-Header' => ['equalTo' => 'test-value']],
-                ]),
-            ],
-        ]);
-        $rawResult = file_get_contents(
-            "http://" . self::$host . ":" . self::$httpPort . "/__admin/requests/count",
-            false,
-            $verifyContext
-        );
-        $this->assertNotFalse($rawResult, 'Failed to reach WireMock admin API');
-        $result = json_decode($rawResult, true);
-        $this->assertGreaterThanOrEqual(1, $result['count'], "Custom header should be present on API call");
+        $opts = new TransportOptions(defaultHeaders: ['X-Custom' => 'value']);
+        $result = $opts->toGuzzleOptions();
+        $this->assertSame(['X-Custom' => 'value'], $result['headers']);
     }
 
-    public function testProxyUrl(): void
+    public function testInsecureTakesPrecedenceOverCaCert(): void
     {
-        $zitadel = Zitadel::withAccessToken(
-            "http://wiremock:8080",
-            "test-token",
-            new TransportOptions(proxyUrl: "http://" . self::$host . ":" . self::$proxyPort),
-        );
-        $this->assertInstanceOf(Zitadel::class, $zitadel);
-        $zitadel->settings->getGeneralSettings();
+        $opts = new TransportOptions(caCertPath: '/path/to/ca.pem', insecure: true);
+        $result = $opts->toGuzzleOptions();
+        $this->assertFalse($result['verify']);
     }
 
-    public function testNoCaCertFails(): void
+    public function testDefaultsFactory(): void
     {
-        $this->expectException(Exception::class);
-        Zitadel::withClientCredentials(
-            "https://" . self::$host . ":" . self::$httpsPort,
-            "dummy-client",
-            "dummy-secret",
-        );
+        $opts = TransportOptions::defaults();
+        $this->assertSame([], $opts->defaultHeaders);
+        $this->assertNull($opts->caCertPath);
+        $this->assertFalse($opts->insecure);
+        $this->assertNull($opts->proxyUrl);
     }
 }
